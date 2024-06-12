2cb4b1de5cdeb07481be731a08873d7301682dd9
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-06-12 02:41:59 -0700

    Practice001

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // Write a Js Program Using Function to Check Whether the number is even or odd?
 
- 
+function even(a){
+    if (a%2 == 0){
+        console.log("Number is Even")
+    }
+    else{
+        console.log("Number is Odd")
+    }
+}
 
-
+even(3);
