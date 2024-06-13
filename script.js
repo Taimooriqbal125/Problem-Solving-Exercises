21125e5787774495d3f9bc5e34a1f067fe40e80d
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-06-13 11:59:27 -0700

    Practice 002

--- script.js
+++ script.js
@@ -10,3 +10,42 @@ function even(a){
 }
 
 even(3);
+
+// Write a JS program using IF-ELSe to check whether a number is negative, positive or zero ?
+
+var number = 2;
+if (number < 0){
+    console.log("Number is Negative ") }
+else if(number > 0){
+    console.log("Number is Positive")
+} 
+else{
+console.log("Zero")
+}
+
+// Write a js program using if else to check whether a character is uppercase or lowercase alphabet?
+
+var char = "D";
+if (char <= "Z"){
+    console.log("UpperCase")
+}
+else{
+    console.log("LowerCase")
+}
+
+// Write a js program to print all alphabets from a to z. - using while loop?
+
+var ch = "abcdefghijklmnopqrstuvwxyz";
+var i = 0;
+while (i < ch.length) {
+    console.log(ch[i]);
+    i++
+ }
+
+//  Write a Js program to write all odd numbers from 0 to 100 ?
+
+var oddNumbers = 1;
+while (oddNumbers <= 100){
+    console.log(oddNumbers)
+    oddNumbers+= 2
+}
